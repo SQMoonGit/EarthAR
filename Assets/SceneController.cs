b0272b28033c999fdf710154a82abf10c656da08
ShockingIce <hykmoon@gmail.com>
2018-04-03 14:27:49 -0500

    New AR app

---
+++ Assets/SceneController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class SceneController : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        QuitOnConnectionErrors();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Session.Status != SessionStatus.Tracking)
+        {
+            const int lostTrackingSleepTimeout = 15;
+            Screen.sleepTimeout = lostTrackingSleepTimeout;
+            return;
+        }
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+	}
+
+    void QuitOnConnectionErrors()
+    {
+        // Do not update if ARCore is not tracking.
+        if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
+            StartCoroutine(CodelabUtils.ToastAndExit("Camera permission is needed to run this application.", 5));
+        else if (Session.Status.IsError())
+            StartCoroutine(CodelabUtils.ToastAndExit("ARCore encountered a problem connecting. Please restart the app.", 5));
+    }
+}
